4959a60a35b2770227f3e036ac587609f6a4416d
papajones03 <papajones03@gmail.com>
2020-05-14 06:04:52 +0100

    Queries for delete jobapps by jobid and update queries for jobs table

--- freelance-ejb/src/java/constants/SqlQueryConstants.java
+++ freelance-ejb/src/java/constants/SqlQueryConstants.java
@@ -23,15 +23,17 @@ public class SqlQueryConstants {
     public static final String FETCH_ALL_PROVIDERS = "SELECT p FROM Provider p";
     public static final String FETCH_ALL_FREELANCERS = "SELECT f FROM Freelancer f";
     public static final String FETCH_ALL_USERNAMES = "SELECT u.username FROM Users u";
-    
     public static final String DELETE_PROVIDER_BY_ID="DELETE FROM Provider p WHERE p.pid = :pid";
     public static final String DELETE_FREELANCER_BY_ID="DELETE FROM Freelancer f WHERE f.uid = :uid";
     
     public static final String DELETE_USER_BY_ID="DELETE FROM Users u WHERE u.id = :id";
     
     public static final String DELETE_JOB_BY_JOBID="DELETE FROM Jobs j WHERE j.jobid = :jobid";
+    public static final String DELETE_JOBAPPS_BY_JOBID="DELETE FROM Jobapps j WHERE j.jobid = :jobid";
      public static final String DELETE_JOBS_BY_PROVID = "DELETE FROM Jobs j WHERE j.createdby = :createdby";
 
     public static final String DELETE_USER_FROM_JOBAPPS="DELETE FROM Jobapps j WHERE j.fid = :fid";
+    
+    public static final String UPDATE_JOBS_BY_FREELANCE_ID="UPDATE Jobs j SET j.acceptedby = :fid, j.jobstatus = :status WHERE j.jobid = :jobid ";
 
 }
