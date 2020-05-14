978bba3924ea7e10dcc34ce934aa5e4d28fd0cca
papajones03 <papajones03@gmail.com>
2020-05-14 19:17:23 +0100

    Update SqlDbTemplate.sql

--- freelance-ejb/src/java/sql/SqlDbTemplate.sql
+++ freelance-ejb/src/java/sql/SqlDbTemplate.sql
@@ -10,10 +10,6 @@
 
 create database dev;
 use dev;
-create table user( username varchar(20), 
-password varchar(20));
-
-use dev;
 CREATE TABLE users (
 id bigint primary key,
 username VARCHAR(30) unique,
@@ -34,12 +30,30 @@ FOREIGN KEY (pid) REFERENCES users(id)
 
 CREATE TABLE freelancer (
 uid bigint primary key,
+amount INT(6) DEFAULT 0,
 skills VARCHAR(500),
 message VARCHAR(500),
 FOREIGN KEY (uid) REFERENCES users(id)
 );
-select * from users;
-use dev;
-drop table freelancer;
-drop table provider;
-drop table users;
+
+CREATE TABLE jobs (
+jobid bigint primary key,
+title VARCHAR(30) NOT NULL,
+skills VARCHAR(30),
+description VARCHAR(64) NOT NULL,
+payment INT(5) NOT NULL,
+createdby bigint,
+acceptedby bigint,
+jobstatus VARCHAR(30) NOT NULL,
+FOREIGN KEY (createdby) REFERENCES provider(pid),
+FOREIGN KEY (acceptedby) REFERENCES freelancer(uid)
+);
+
+CREATE TABLE jobapps (
+aid bigint NOT NULL AUTO_INCREMENT primary key,
+jobid BIGINT DEFAULT 0  NOT NULL,
+fid BIGINT DEFAULT 0  NOT NULL,
+FOREIGN KEY (jobid) REFERENCES jobs(jobid),
+FOREIGN KEY (fid) REFERENCES freelancer(uid)
+);
+
